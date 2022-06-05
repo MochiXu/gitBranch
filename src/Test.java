b32e7158a250d582b4ad4d5c9258adc747e5d768
Mochi Xu <mirage_mc@icloud.com>
2022-06-05 19:15:16 +0800

    dev1 commit 1

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String args[]){
         //main init
         System.out.println("main init");
+        //dev1
+        System.out.println("dev1 commit 1");
     }
 }
